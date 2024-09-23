67fefce6e108f8957624fde6f456ba7751926d53
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-09-23 10:05:39 -0300

    feat: Add getRow function to retrieve a specific row from Pascal's Triangle

---
+++ typescript/getRow.ts
@@ -0,0 +1,26 @@
+function pascalTriangle(numRows: number): number[][] {
+    let result: number[][] = [];
+
+    for (let index = 0; index < numRows; index++) {
+        result.push(Array.from({ length: index + 1 }).map((_, itemIndex, array) => {
+            const previousRow = result[index - 1]
+
+            if (!previousRow || previousRow.length === 1) {
+                return 1
+            }
+
+            if (itemIndex === 0 || itemIndex === array.length - 1) {
+                return 1
+            }
+
+           return previousRow[itemIndex] + previousRow[itemIndex -1];
+        }))
+    }
+
+    return result
+};
+
+function getRow(rowIndex: number): number[] {
+    const rows = pascalTriangle(rowIndex + 1)
+    return rows[rowIndex]
+};
