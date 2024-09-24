5e4d92ae6af05db21fb3e5b37c10dbe5e5e669a6
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-09-24 13:54:07 -0300

    Refactor getConcatenation function to create a new array with the concatenated elements of the input array

---
+++ typescript/go/getConcatenation.go
@@ -0,0 +1,13 @@
+func getConcatenation(nums []int) []int {
+    result := make([]int, len(nums) * 2)
+
+    for i := 0; i < len(nums); i++ {
+        result[i] = nums[i]
+    }
+
+    for i := 0; i < len(nums); i++ {
+        result[i + len(nums)] = nums[i]
+    }
+
+    return result
+}
